e14eb485c8113b9e2ffd88c3e5606a5674688b23
Vikram Naik <26vikramnaik@gmail.com>
2023-11-29 23:08:59 +0530

    react typical project folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import Starter from './useEffect/cleanup-function'
+// import Starter from './useEffect/cleanup-function'
+
+import Navbar from './project-structure/navbar'
+/* import About from './project-structure/pages/About'
+import Home from './project-structure/pages/Home' */
+
+import { Home, About } from './project-structure/pages'
 
 function App() {
   return (
     <div className="container">
-      <Starter />
+      {/* <Starter /> */}
+      <Navbar />
+      <Home />
+      <About />
     </div>
   )
 }
---
+++ src/project-structure/Navbar/Navbar.jsx
@@ -0,0 +1,4 @@
+const Navbar = () => {
+  return <h3>Navbar</h3>
+}
+export default Navbar
