ccdb4cf750c3fc1047216f98e3e0102931697cbb
Vikram Naik <26vikramnaik@gmail.com>
2023-11-30 23:34:03 +0530

    multiple inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 // import Example from './project-structure/Example'
 // import List from './leverage-javascript/List'
 
-import UserChallenge from './forms/user-challenge'
+import MultipleInputs from './forms/multiple-input'
 
 function App() {
   return (
     <div className="container">
       {/* <Example /> */}
       {/* <List /> */}
-      <UserChallenge />
+      <MultipleInputs />
     </div>
   )
 }
