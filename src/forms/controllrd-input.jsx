29797a4db9811e940bfdc8c722516023de635734
Vikram Naik <26vikramnaik@gmail.com>
2023-11-30 21:27:55 +0530

    form example

--- src/forms/controllrd-input.jsx
+++ src/forms/controllrd-input.jsx
@@ -1,18 +1,56 @@
+import { useState } from 'react'
+
 const ControlledInputs = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  /* const handleChange = (e) => {
+    const { name, value } = e.target
+    console.log(value)
+    console.log(name)
+    setName(value)
+  } */
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // do something
+    console.log(name, email)
+  }
+
   return (
-    <form action="" className="form">
+    <form action="" className="form" onSubmit={handleSubmit}>
       <h4>controlled input</h4>
       <div className="form-row">
         <label htmlFor="name" className="form-label">
           name
         </label>
-        <input type="text" name="" id="name" className="form-input" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className="form-input"
+          value={name}
+          onChange={(e) => {
+            const { name, value } = e.target
+            setName(value)
+          }}
+        />
       </div>
       <div className="form-row">
-        <label htmlFor="" className="form-label">
+        <label htmlFor="email" className="form-label">
           email
         </label>
-        <input type="email" name="" id="email" className="form-input" />
+        <input
+          type="email"
+          id="email"
+          className="form-input"
+          name="email"
+          value={email}
+          onChange={(e) => {
+            const { name, value } = e.target
+            setEmail(value)
+          }}
+        />
       </div>
       <button type="submit" className="btn btn-block">
         submit
