0a9939ff478d2d12f599b1571e61c951be7cee89
Vikram Naik <26vikramnaik@gmail.com>
2023-12-01 22:19:00 +0530

    custom hooks basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 // import Example from './project-structure/Example'
 // import List from './leverage-javascript/List'
 
+import ToggleExample from './custom-hools/toggle'
+
 // import OtherInputs from './forms/other-inputs'
-import UseRefBasics from './useRef/useRef-basics'
 
 function App() {
   return (
     <div className="container">
       {/* <Example /> */}
       {/* <List /> */}
-      <UseRefBasics />
+      <ToggleExample />
     </div>
   )
 }
