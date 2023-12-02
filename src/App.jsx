d1e8874b1f3ffed07044dab7bcd69de098a6a432
Vikram Naik <26vikramnaik@gmail.com>
2023-12-02 15:55:09 +0530

    lower state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 // import Example from './project-structure/Example'
 // import List from './leverage-javascript/List'
 
-import Navbar from './context-api/Navbar'
-import AppContext, { useGlobalContext } from './global-context-api/context'
-import ReducerBasics from './useReducer/useReducer'
-
 // import OtherInputs from './forms/other-inputs'
+import LowerState from './performance/lower-state'
 
 function App() {
   // const { name } = useGlobalContext()
@@ -13,7 +10,7 @@ function App() {
     <div className="container">
       {/* <Example /> */}
       {/* <List /> */}
-      <ReducerBasics />
+      <LowerState />
     </div>
     // <AppContext>
     //   <Navbar />
