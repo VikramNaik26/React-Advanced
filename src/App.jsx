2fdc175b7890625182e702ed869d74950025302d
Vikram Naik <26vikramnaik@gmail.com>
2023-12-02 12:56:21 +0530

    global context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 // import Example from './project-structure/Example'
 // import List from './leverage-javascript/List'
 
-import Navbar from "./context-api/Navbar"
-
-
+import Navbar from './context-api/Navbar'
+import AppContext, { useGlobalContext } from './global-context-api/context'
 
 // import OtherInputs from './forms/other-inputs'
 
 function App() {
+  // const { name } = useGlobalContext()
   return (
-    <div className="container">
-      {/* <Example /> */}
-      {/* <List /> */}
-      <Navbar/>
-    </div>
+    // <div className="container">
+    //   {/* <Example /> */}
+    //   {/* <List /> */}
+    //   <Navbar/>
+    // </div>
+    <AppContext>
+      <Navbar />
+    </AppContext>
   )
 }
 
